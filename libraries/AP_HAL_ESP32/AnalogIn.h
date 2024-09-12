d0bb6556e7f5ae5a791b0b02eca3010413a0f0cc
ARg <aromanov@comped.it>
2024-09-12 16:00:06 +0200

    AP_HAL_ESP32 ADC code porting to new driver

--- libraries/AP_HAL_ESP32/AnalogIn.h
+++ libraries/AP_HAL_ESP32/AnalogIn.h
@@ -19,18 +19,25 @@
 
 #include "AP_HAL_ESP32.h"
 
-#if HAL_USE_ADC == TRUE && !defined(HAL_DISABLE_ADC_DRIVER)
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+
+#if HAL_USE_ADC
+
+//&& !defined(HAL_DISABLE_ADC_DRIVER)
 
 #define ANALOG_MAX_CHANNELS 8
 
 namespace ESP32
 {
 
+
 class AnalogSource : public AP_HAL::AnalogSource
 {
 public:
     friend class AnalogIn;
-    AnalogSource(int16_t ardupin,int16_t pin,float scaler, float initial_value, uint8_t unit);
+    AnalogSource( adc_oneshot_unit_handle_t adc_handle, int16_t ardupin, adc_channel_t channel, float scaler, float initial_value, adc_unit_t unit);
     float read_average() override;
     float read_latest() override;
     bool set_pin(uint8_t p) override;
@@ -41,12 +48,16 @@ public:
     void set_settle_time(uint16_t settle_time_ms) {}
 
 private:
+
+    adc_oneshot_unit_handle_t _adc_handle;
+    adc_cali_handle_t _adc_cali_handle;
+
     //ADC number (1 or 2). ADC2 is unavailable when WIFI on
-    uint8_t _unit;
+    adc_unit_t _unit;
 
     //adc Pin number (1-8)
     // gpio-adc lower level pin name
-    int16_t _pin;
+    adc_channel_t _channel;
 
     //human readable Pin number used in ardu params
     int16_t _ardupin;
@@ -66,6 +77,8 @@ private:
 
     void _add_value();
 
+    int adc_read();
+
     HAL_Semaphore _semaphore;
 };
 
@@ -76,11 +89,13 @@ public:
 
     void init() override;
     AP_HAL::AnalogSource* channel(int16_t pin) override;
-    void _timer_tick();
-    float board_voltage() override
-    {
-        return _board_voltage;
-    }
+    void _timer_tick(void);
+    void timer_tick_adc(uint8_t index);
+    float board_voltage(void) override { return _board_voltage; }
+    float servorail_voltage(void) override { return _servorail_voltage; }
+    uint16_t power_status_flags(void) override { return _power_flags; }
+    uint16_t accumulated_power_status_flags(void) const override { return _accumulated_power_flags; }
+
     static int8_t find_pinconfig(int16_t ardupin);
 
 private:
@@ -88,15 +103,21 @@ private:
 
     uint32_t _last_run;
     float _board_voltage;
+    float _servorail_voltage;
+    float _rssi_voltage;
+    uint16_t _power_flags;
+    uint16_t _accumulated_power_flags;  // bitmask of all _power_flags ever set
+
+    adc_oneshot_unit_handle_t _adc_handle;
 
     struct pin_info {
-        uint8_t channel;  // adc1 pin offset
+        uint8_t channel;
         float scaling;
         uint8_t ardupin; // eg 3 , as typed into an ardupilot parameter
-        uint8_t gpio; // eg 32 for D32 esp pin number/s
     };
 
     static const pin_info pin_config[];
+
 };
 
 }
