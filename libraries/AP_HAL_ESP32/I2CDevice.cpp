cbfbf7b826b5e7ad7c6c253a019b9268fb80f9be
ARg <aromanov@comped.it>
2024-09-25 15:20:37 +0200

    AP_HAL_ESP32: i2c timeout fix and more error checking

--- libraries/AP_HAL_ESP32/I2CDevice.cpp
+++ libraries/AP_HAL_ESP32/I2CDevice.cpp
@@ -21,15 +21,37 @@
 
 using namespace ESP32;
 
+//#define BUSDEBUG 1
+
 #define MHZ (1000U*1000U)
 #define KHZ (1000U)
+#define I2C_TIMEOUT_VALUE_MS (50)
 
 I2CBusDesc i2c_bus_desc[] = { HAL_ESP32_I2C_BUSES };
 
 I2CBus I2CDeviceManager::businfo[ARRAY_SIZE(i2c_bus_desc)];
 
+#ifdef BUSDEBUG
+
+#define ESP_CHK( fn, args ...) \
+{ \
+    int err = fn(args); \
+    if (err != ESP_OK) {\
+        printf("%s:%d :",  __PRETTY_FUNCTION__, __LINE__); \
+        printf("error %d\n", err); \
+    } \
+}
+#else
+   #define ESP_CHK( fn, args ...) fn(args)
+#endif
+
 I2CDeviceManager::I2CDeviceManager(void)
 {
+
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
+
     for (uint8_t i=0; i<ARRAY_SIZE(i2c_bus_desc); i++) {
         if (i2c_bus_desc[i].soft) {
             businfo[i].sw_handle.sda = i2c_bus_desc[i].sda;
@@ -51,9 +73,12 @@ I2CDeviceManager::I2CDeviceManager(void)
             businfo[i].port = p;
             businfo[i].bus_clock = i2c_bus_desc[i].speed;
             businfo[i].soft = false;
-            i2c_param_config(p, &i2c_bus_config);
-            i2c_driver_install(p, I2C_MODE_MASTER, 0, 0, ESP_INTR_FLAG_IRAM);
-            i2c_filter_enable(p, 7);
+            ESP_CHK(i2c_param_config, p, &i2c_bus_config);
+            ESP_CHK(i2c_driver_install, p, I2C_MODE_MASTER, 0, 0, ESP_INTR_FLAG_IRAM);
+            ESP_CHK(i2c_filter_enable, p, 7);
+
+            int tm = 19; // about 10 ms, power of two APB CLK
+            ESP_CHK(i2c_set_timeout, p, tm);
         }
     }
 }
@@ -67,11 +92,17 @@ I2CDevice::I2CDevice(uint8_t busnum, uint8_t address, uint32_t bus_clock, bool u
     set_device_address(address);
     asprintf(&pname, "I2C:%u:%02x",
              (unsigned)busnum, (unsigned)address);
+#ifdef BUSDEBUG
+    printf("NEW %s\n", pname);
+#endif
 }
 
 I2CDevice::~I2CDevice()
 {
-    free(pname);
+#ifdef BUSDEBUG
+    printf("DEL %s\n", pname);
+#endif
+   free(pname);
 }
 
 bool I2CDevice::transfer(const uint8_t *send, uint32_t send_len,
@@ -87,7 +118,7 @@ bool I2CDevice::transfer(const uint8_t *send, uint32_t send_len,
         uint8_t flag_wr = (recv_len == 0 || recv == nullptr) ? I2C_NOSTOP : 0;
         if (send_len != 0 && send != nullptr) {
             //tx with optional rx (after tx)
-            i2c_write_bytes(&bus.sw_handle,
+            ESP_CHK(i2c_write_bytes, &bus.sw_handle,
                             _address,
                             send,
                             send_len,
@@ -96,7 +127,7 @@ bool I2CDevice::transfer(const uint8_t *send, uint32_t send_len,
         if (recv_len != 0 && recv != nullptr) {
             //rx only or rx after tx
             //rx separated from tx by (re)start
-            i2c_read_bytes(&bus.sw_handle,
+            ESP_CHK(i2c_read_bytes, &bus.sw_handle,
                            _address,
                            (uint8_t *)recv, recv_len, 0);
         }
@@ -105,25 +136,36 @@ bool I2CDevice::transfer(const uint8_t *send, uint32_t send_len,
         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
         if (send_len != 0 && send != nullptr) {
             //tx with optional rx (after tx)
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (_address << 1) | I2C_MASTER_WRITE, true);
-            i2c_master_write(cmd, (uint8_t*)send, send_len, true);
+            ESP_CHK(i2c_master_start, cmd);
+            ESP_CHK(i2c_master_write_byte, cmd, (_address << 1) | I2C_MASTER_WRITE, true);
+            ESP_CHK(i2c_master_write, cmd, (uint8_t*)send, send_len, true);
         }
         if (recv_len != 0 && recv != nullptr) {
             //rx only or rx after tx
             //rx separated from tx by (re)start
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (_address << 1) | I2C_MASTER_READ, true);
-            i2c_master_read(cmd, (uint8_t *)recv, recv_len, I2C_MASTER_LAST_NACK);
+            ESP_CHK(i2c_master_start, cmd);
+            ESP_CHK(i2c_master_write_byte, cmd, (_address << 1) | I2C_MASTER_READ, true);
+            ESP_CHK(i2c_master_read, cmd, (uint8_t *)recv, recv_len, I2C_MASTER_LAST_NACK);
         }
-        i2c_master_stop(cmd);
+        ESP_CHK(i2c_master_stop, cmd);
 
-        TickType_t timeout = 1 + 16L * (send_len + recv_len) * 1000 / bus.bus_clock / portTICK_PERIOD_MS;
-        for (int i = 0; !result && i < _retries; i++) {
-            result = (i2c_master_cmd_begin(bus.port, cmd, timeout) == ESP_OK);
+        TickType_t timeout = I2C_TIMEOUT_VALUE_MS; //1 + 16L * (send_len + recv_len) * 1000 / bus.bus_clock / portTICK_PERIOD_MS;
+
+        for (int i = 0; i < _retries; i++) {
+            int ret = i2c_master_cmd_begin(bus.port, cmd, timeout);
+            result = ( ret == ESP_OK);
             if (!result) {
-                i2c_reset_tx_fifo(bus.port);
-                i2c_reset_rx_fifo(bus.port);
+#ifdef BUSDEBUG
+                printf("i2c_master_cmd_begin failed with %d, bus.port = %x, addr = 0x%x, timeout = %ld\n", ret, bus.port, _address, timeout);
+#endif
+                ESP_CHK(i2c_reset_tx_fifo, bus.port);
+                ESP_CHK(i2c_reset_rx_fifo, bus.port);
+            }
+            else {
+#ifdef BUSDEBUG
+                printf("i2c_master_cmd_begin bus.port = %x, addr = 0x%x, timeout = %ld : OK\n", bus.port, _address, timeout);
+#endif
+               break;
             }
         }
 
@@ -138,6 +180,9 @@ bool I2CDevice::transfer(const uint8_t *send, uint32_t send_len,
 */
 AP_HAL::Device::PeriodicHandle I2CDevice::register_periodic_callback(uint32_t period_usec, AP_HAL::Device::PeriodicCb cb)
 {
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
     return bus.register_periodic_callback(period_usec, cb, this);
 }
 
@@ -147,6 +192,9 @@ AP_HAL::Device::PeriodicHandle I2CDevice::register_periodic_callback(uint32_t pe
 */
 bool I2CDevice::adjust_periodic_callback(AP_HAL::Device::PeriodicHandle h, uint32_t period_usec)
 {
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
     return bus.adjust_timer(h, period_usec);
 }
 
@@ -156,6 +204,9 @@ I2CDeviceManager::get_device(uint8_t bus, uint8_t address,
                              bool use_smbus,
                              uint32_t timeout_ms)
 {
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
     if (bus >= ARRAY_SIZE(i2c_bus_desc)) {
         return AP_HAL::OwnPtr<AP_HAL::I2CDevice>(nullptr);
     }
@@ -176,6 +227,9 @@ uint32_t I2CDeviceManager::get_bus_mask(void) const
 */
 uint32_t I2CDeviceManager::get_bus_mask_internal(void) const
 {
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
     uint32_t result = 0;
     for (size_t i = 0; i < ARRAY_SIZE(i2c_bus_desc); i++) {
         if (i2c_bus_desc[i].internal) {
@@ -190,5 +244,8 @@ uint32_t I2CDeviceManager::get_bus_mask_internal(void) const
 */
 uint32_t I2CDeviceManager::get_bus_mask_external(void) const
 {
+#ifdef BUSDEBUG
+    printf("%s:%d \n", __PRETTY_FUNCTION__, __LINE__);
+#endif
     return get_bus_mask() & ~get_bus_mask_internal();
 }
